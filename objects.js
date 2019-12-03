78bd45eeafecf65933469f277af67a7b57448a17
April Escobar <aprilfaith.e@gmail.com>
2019-12-03 20:31:04 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,13 +1,13 @@
 var playlist = {
   Drake: "Views"
-}
+};
 
 function updatePlaylist(playlist, artistName, songTitle) {
   playlist[artistName] = songTitle;
-  return playlist
+  return playlist;
 }
 
 function removeFromPlaylist(playlist, artistName) {
   delete playlist.artistName;
-  return playlist
+  return playlist;
 }
